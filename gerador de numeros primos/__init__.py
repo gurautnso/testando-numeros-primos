a406d228294f45fe8113d686497d3832a8450bc6
Guilherme Simões Rosa <133579859+gurautnso@users.noreply.github.com>
2023-12-08 23:03:54 -0300

    save005

--- gerador de numeros primos/__init__.py
+++ gerador de numeros primos/__init__.py
@@ -40,6 +40,9 @@ for c1 in range(2, n+1):
     if n > 38:
         if c1 % nl == 0:
             print()
+<<<<<<< HEAD
+>>>>>>> parent of e0515c0 (save003)
+=======
 >>>>>>> parent of e0515c0 (save003)
     for c2 in range(2, c1+1):
         if c1/2 < c2:
@@ -54,6 +57,7 @@ for c1 in range(2, n+1):
             a.close()
             break
 <<<<<<< HEAD
+<<<<<<< HEAD
     if c1 % nl == 0:
         a = open('numbers.txt', 'at')
         a.write(f'\n')
@@ -62,5 +66,7 @@ a = open('numbers.txt', 'at')
 a.write(f'\nTotal de primos:{primos}')
 a.close()
 =======
+=======
+>>>>>>> parent of e0515c0 (save003)
 print(f'\nTotal de primos:{primos}')
 >>>>>>> parent of e0515c0 (save003)
