1dded12d0c49a4dc2c9009eaccd762611b31e687
Guilherme Simões Rosa <133579859+gurautnso@users.noreply.github.com>
2023-12-08 23:06:04 -0300

    save006

--- gerador de numeros primos/__init__.py
+++ gerador de numeros primos/__init__.py
@@ -33,40 +33,28 @@ if n > 114:
                 nl = 22
                 ne = 6
 print(f'{"1":>{ne}}', end=' ')
-<<<<<<< HEAD
 for c1 in range(n, n+1):
-=======
-for c1 in range(2, n+1):
-    if n > 38:
+    for c1 in range(2, n+1):
+        if n > 38:
+            if c1 % nl == 0:
+                print()
+        for c2 in range(2, c1+1):
+            if c1/2 < c2:
+                a = open('numbers.txt', 'at')
+                a.write(f'\033[1:31m{c1:>{ne}}\033[m;')
+                a.close()
+                primos += 1
+                break
+            if c1 % c2 == 0:
+                a = open('numbers.txt', 'at')
+                a.write(f'{c1:>{ne}}')
+                a.close()
+                break
         if c1 % nl == 0:
-            print()
-<<<<<<< HEAD
->>>>>>> parent of e0515c0 (save003)
-=======
->>>>>>> parent of e0515c0 (save003)
-    for c2 in range(2, c1+1):
-        if c1/2 < c2:
             a = open('numbers.txt', 'at')
-            a.write(f'\033[1:31m{c1:>{ne}}\033[m;')
+            a.write(f'\n')
             a.close()
-            primos += 1
-            break
-        if c1 % c2 == 0:
-            a = open('numbers.txt', 'at')
-            a.write(f'{c1:>{ne}}')
-            a.close()
-            break
-<<<<<<< HEAD
-<<<<<<< HEAD
-    if c1 % nl == 0:
-        a = open('numbers.txt', 'at')
-        a.write(f'\n')
-        a.close()
 a = open('numbers.txt', 'at')
 a.write(f'\nTotal de primos:{primos}')
 a.close()
-=======
-=======
->>>>>>> parent of e0515c0 (save003)
 print(f'\nTotal de primos:{primos}')
->>>>>>> parent of e0515c0 (save003)
